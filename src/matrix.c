30725598980d782c88ea419c60fc0110b71dd56f
Kofi Hannam <khannam@students.kennesaw.edu>
2026-04-24 17:26:50 -0400

    Enable O3 optimizations and adjust loops for better cache locality

--- src/matrix.c
+++ src/matrix.c
@@ -3,8 +3,8 @@
 #include "matrix-common.h"
 
 void gemm(Matrix *a, Matrix *b, Matrix *c) {
-  for (size_t lane = 0; lane < a->columns; lane++)
-    for (size_t row = 0; row < a->rows; row++)
+  for (size_t row = 0; row < a->rows; row++)
+    for (size_t lane = 0; lane < a->columns; lane++)
       for (size_t column = 0; column < b->columns; column++)
         c->values[row * b->columns + column] +=
             a->values[row * a->columns + lane] * b->values[lane * b->columns + column];
